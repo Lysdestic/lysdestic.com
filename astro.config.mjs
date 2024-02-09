d0446d64006cf1e6aa6326aa30f04c6137be6d0c
Lysdestic <github@lysdestic.com>
2024-02-08 18:20:50 -0800

    Upgrade astro-icon to V1 per https://www.astroicon.dev/guides/upgrade/v1/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
-import netlify from '@astrojs/netlify/functions'
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  integrations: [icon()],
 })

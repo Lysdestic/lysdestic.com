d6c19cc104b4a8367291dda634f12f5284847d87
lysdestic <github@lysdestic.com>
2024-02-08 22:59:43 -0800

    dir restructure & removed netflify so npm run build accomodates my use case, ditched orphaned images

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,7 +1,6 @@
 // References libraries and icons found through Iconify (https://icon-sets.iconify.design/) as used by astro-icon
 
 export const icons = {
-  netlify: 'simple-icons:netlify',
   github: 'ion:logo-github',
   githubPages: 'uil:github-alt',
 }
